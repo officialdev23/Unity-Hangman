14fca648629fe3c626e6782ab9a38eaa2b783bcb
Amal4m41 <amalsunil117@gmail.com>
2021-06-10 17:39:23 +0400

    added feature to get user input through keystroke

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,6 +8,7 @@ public class GameController : MonoBehaviour
     public Text timeField;
     public Text wordToFindField;
 
+
     private float time;  //in C# when the value is empty it's assigned 0 by default.
     private string[] wordsLocal= { "NEYMAR JR","RONALDO JR"};
     //private int[] myNums = { 1, 2, 3, 4 };
@@ -30,13 +31,14 @@ public class GameController : MonoBehaviour
             if(char.IsWhiteSpace(letter))
             {
                 //hiddenWord += ' ';
-                hiddenWord += "  ";  //string because two whitespace is added
+                hiddenWord += " ";  //string because two whitespace is added
                 continue;  //skip the rest of the statements a cause the next iteration to take place.
             }
-            hiddenWord += "_ ";
+            hiddenWord += "_";
         }
-        hiddenWord = hiddenWord.Substring(0,hiddenWord.Length-1);  //remove the last char cuz it's space
+        //hiddenWord = hiddenWord.Substring(0,hiddenWord.Length-1);  //remove the last char cuz it's space
 
+        
         wordToFindField.text = hiddenWord;
         //Debug.Log(hiddenWord);
 
@@ -51,5 +53,62 @@ public class GameController : MonoBehaviour
         timeField.text = ((int)time).ToString();  //just displaying the time in seconds(ignoring ms)
         //Debug.Log(Time.deltaTime); approx 0.02s, i.e. 1f in 0.02s which means it's 50fps
 
+    }
+
+
+    //Will be called every single time when a GUI event happens(i.e. keystrokes)
+    private void OnGUI()
+    {
+        Event e = Event.current;   //returns the current event being processed.
+
+        //Method-1
+        /*
+        if(e.type == EventType.KeyDown && e.keyCode.ToString().Length==1) //e.keyCode.ToString().Length==1 to prevent the "None" 
+        {
+            //Debug.Log(e.type);
+            //Debug.Log(e.keyCode); //on one key stroke eg: 'Q' we get 'Q' followed by 'None'.
+            string keyPressed = e.keyCode.ToString();
+            string result = "";
+            if(chosenWord.Contains(keyPressed))
+            {
+                if (hiddenWord.Contains(keyPressed))
+                {
+                    Debug.Log("ALREADY ENTERED!");
+                }
+                else
+                {
+                    for(int i = 0; i < chosenWord.Length; i++)
+                    {
+                        if (chosenWord.ToUpper()[i].ToString() == keyPressed)
+                        {
+                            result+=keyPressed;
+                        }
+                        else
+                        {
+                            result += hiddenWord[i];
+                        }
+                    }
+                    hiddenWord = result;
+                    wordToFindField.text = hiddenWord;
+                }
+            }
+            else
+            {
+                Debug.Log("NOPE!");
+            }
+        }
+        */
+
+        if (e.type == EventType.KeyDown && e.keyCode.ToString().Length == 1) //e.keyCode.ToString().Length==1 to prevent the "None" 
+        {
+            //Debug.Log(e.type);
+            //Debug.Log(e.keyCode); //on one key stroke eg: 'Q' we get 'Q' followed by 'None'.
+            string keyPressed = e.keyCode.ToString();
+            string result = "";
+            while (chosenWord.IndexOf(keyPressed) > -1)  //indexOf gives the first found index of the string, if not found then -1
+            {
+
+            }
+        }
     }
 }
