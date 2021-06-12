c3283136a72e05a3fe3f2ed460cc0751b99c2d98
Amal4m41 <amalsunil117@gmail.com>
2021-06-12 20:04:39 +0400

    complete game logic for win and lose along with the hangman visuals

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,16 +5,19 @@ using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
+    public GameObject[] hangman;   //array of game objects to store the hangman related game objects.
     public Text timeField;
     public Text wordToFindField;
-
+    public GameObject winText;  //to control the win screen
+    public GameObject loseText;
 
     private float time;  //in C# when the value is empty it's assigned 0 by default.
     private string[] wordsLocal= { "NEYMAR JR","RONALDO JR"};
     //private int[] myNums = { 1, 2, 3, 4 };
     private string chosenWord;
     private string hiddenWord;
-
+    private int countChance = 0;// we have 5 chances(6 will end the game with losing screen), as our hangman has 6 body elements.
+    private bool gameEnd = false;
 
     // Start is called before the first frame update
     void Start()
@@ -48,10 +51,13 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //deltaTime returns the interval in seconds from the last frame to the current one.
-        time += Time.deltaTime;   //i.e if we have 1fps deltaTime=1; 10fps deltaTime=0.1
-        timeField.text = ((int)time).ToString();  //just displaying the time in seconds(ignoring ms)
-        //Debug.Log(Time.deltaTime); approx 0.02s, i.e. 1f in 0.02s which means it's 50fps
+        if (gameEnd == false)
+        {
+            //deltaTime returns the interval in seconds from the last frame to the current one.
+            time += Time.deltaTime;   //i.e if we have 1fps deltaTime=1; 10fps deltaTime=0.1
+            timeField.text = ((int)time).ToString();  //just displaying the time in seconds(ignoring ms)
+            //Debug.Log(Time.deltaTime); approx 0.02s, i.e. 1f in 0.02s which means it's 50fps
+        }
 
     }
 
@@ -99,16 +105,50 @@ public class GameController : MonoBehaviour
         }
         */
 
+
+        //Method-2
         if (e.type == EventType.KeyDown && e.keyCode.ToString().Length == 1) //e.keyCode.ToString().Length==1 to prevent the "None" 
         {
             //Debug.Log(e.type);
             //Debug.Log(e.keyCode); //on one key stroke eg: 'Q' we get 'Q' followed by 'None'.
             string keyPressed = e.keyCode.ToString();
-            string result = "";
-            while (chosenWord.IndexOf(keyPressed) > -1)  //indexOf gives the first found index of the string, if not found then -1
-            {
 
+
+            if (!hiddenWord.Contains(keyPressed) && !chosenWord.Contains(keyPressed))
+            {
+                //Debug.Log("WRONG LETTER");
+                hangman[countChance].SetActive(true);
+                countChance += 1;
+                if (countChance == hangman.Length)  //if it's equal to 6 then show lose screen
+                {
+                    //Debug.Log("YOU LOSE!");
+                    loseText.SetActive(true);
+                    gameEnd = true;
+                }
             }
+            else if(hiddenWord.Contains(keyPressed))
+            {
+                Debug.Log("ALREADY ENTERED LETTER");
+            }
+            else  //the entered input is a letter in the chosenWord
+            {
+                int index = chosenWord.IndexOf(keyPressed);
+                while (index > -1)  //indexOf gives the first found index of the string, if not found then -1
+                {
+                    hiddenWord = hiddenWord.Substring(0, index) + keyPressed + hiddenWord.Substring(index + 1);
+                    chosenWord= chosenWord.Substring(0, index) + '_' + chosenWord.Substring(index + 1);
+                    index = chosenWord.IndexOf(keyPressed);
+                }
+                wordToFindField.text = hiddenWord;
+
+                if (!hiddenWord.Contains("_"))// if there's no more underscores in hiddenWord
+                {
+                    //Debug.Log("YOU WIN!");
+                    winText.SetActive(true);
+                    gameEnd = true;
+                }
+            }   
+
         }
     }
 }
